4be717351d26830fa3e1b773e04871c9d8e332ae
Gabriele <gabrimassi@rocketmail.com>
2026-01-20 19:50:53 +0100

    Created toggle and delete functionality and better encapsulated todos creation function.

--- src/functions.js
+++ src/functions.js
@@ -1,11 +1,13 @@
 class Todo{
     //constructor(title, description, dueDate, priority, id){
-    constructor(title){
+    constructor(title, id){
         this.title=title;
         /*this.description=description;
-        this.dueDate=dueDate;   //for now avoid date module due to npm breach
-        this.priority=priority;
-        this.id=id;   */
+        this.dueDate=dueDate;
+        this.priority=priority;*/
+        this.id=id;     //uuid dataset
+        //this.done=done ? done:false;
+        this.done=false;
     }
 }
 
@@ -15,11 +17,15 @@ class TodoList{
     }
 
     addTodo(title){
-        this.todos.push(new Todo(title));
+        this.todos.push(new Todo(title, crypto.randomUUID()));
     }
 
     getTodos(){
         return this.todos;
+    }
+
+    removeTodos(id){
+        this.todos = this.todos.filter((todo)=> todo.id!=id);
     }
 }
 
@@ -33,28 +39,60 @@ export function closeForm(form){
     form.hidden=true;
 }
 
-export function createTodo(desc){  //dataset for id
-    //todos.push(new Todo(desc));
-    todoList.addTodo(desc);
-    //console.log("todos: "+todos[0].title);
-}
-
-export function renderTodo(){   //change
-    let tempTodos=todoList.getTodos();
+export function renderTodo(renderElement){   //change
+    const tempTodos=todoList.getTodos();
     console.table(tempTodos);
     const renderFragment=document.createDocumentFragment();
     tempTodos.forEach((elem)=>{
         const todoDiv=document.createElement("div");
         todoDiv.className="todo";
+        todoDiv.dataset.id=elem.id; //remove after testing
+
+        const todoTextContainer=document.createElement("div");
+        todoTextContainer.className="todoTextContainer";
+
+        const todoStatus=document.createElement("input");
+        todoStatus.className="todoStatus";
+        todoStatus.type="checkbox";
+        todoStatus.addEventListener("click", ()=>{
+            toggleStatus(elem.id);
+            renderingArea(renderElement);
+        });
+        todoTextContainer.appendChild(todoStatus);
+        //todoDiv.appendChild(todoStatus);
+
+        //if(elem.done) todoTextContainer.className+=" completed";
+        if(elem.done){
+            todoDiv.className+=" completed";
+            todoStatus.checked=true;
+        }
 
         const todoText = document.createElement("div");
         todoText.className="todoText";
         todoText.textContent=elem.title;
-
-        todoDiv.appendChild(todoText);
+        todoTextContainer.appendChild(todoText);
+        //todoDiv.appendChild(todoText);
+        todoDiv.appendChild(todoTextContainer); //this works with right styling, but better switch to grid? NO
 
         const todoCommands=document.createElement("div");
-        todoCommands.textContent="delete etc...";   //date, complete, delete at least
+        todoCommands.className="todoCommands";
+        //todoCommands.textContent="delete etc...";   //date, complete, delete at least
+        //complete before todo content, maybe priority? and a popup to modify if that's the case
+        const dateSelector=document.createElement("p"); //date type for input (active???), div for output
+        dateSelector.className="dateSelector";
+        dateSelector.textContent="No date";
+        todoCommands.appendChild(dateSelector);
+
+        const deleteButton=document.createElement("p");
+        deleteButton.className="deleteTodo";
+        deleteButton.textContent="Del"; //use array splice? no
+        deleteButton.addEventListener("click", ()=>{
+            todoList.removeTodos(elem.id);
+            renderingArea(renderElement);
+        });
+        todoCommands.appendChild(deleteButton);
+
+        
         todoDiv.appendChild(todoCommands);
 
         renderFragment.appendChild(todoDiv);
@@ -64,13 +102,58 @@ export function renderTodo(){   //change
     return(renderFragment);
 }
 
+//refresh closures, it seems I've used them without actively remembering them
+
+function toggleStatus(id){
+    //console.log("inside toggle function");
+    const elem=todoList.getTodos().find((todo)=>todo.id===id);
+    switch(elem.done){
+        case true:
+            elem.done=false;
+            break;
+        
+        case false:
+            elem.done=true;
+            break;
+    }
+}
+
+function renderingArea(renderElement){
+    renderElement.innerHTML="";
+    renderElement.appendChild(renderTodo(renderElement));
+}
+
 export function createAction(content, form, renderElement){
-    createTodo(content);
+    if(content===""){
+        console.log("empty detected");
+        return;
+    }
+    if(content[0]===" "){
+        if(content.length>1){
+            switch(content[1]){
+                case "":
+                    console.log("space detected");
+                    return;
+                case " ":
+                    console.log("space detected");
+                    return;
+                default: 
+                    break;
+            }
+        }
+        else{
+            console.log("space detected");
+            return;
+        }
+    }
+    //createTodo(content);
+    todoList.addTodo(content);
     //console.log("newText value: "+newText.value);
     console.log("newText value: "+content);
     form.reset();
     closeForm(form);
     //todoContainer.appendChild(renderTodo());    //clean area before
-    renderElement.innerHTML="";
-    renderElement.appendChild(renderTodo());    //clean area before
+    /*renderElement.innerHTML="";
+    renderElement.appendChild(renderTodo(renderElement));*/    //clean area before; pass renderElement?
+    renderingArea(renderElement);
 }
